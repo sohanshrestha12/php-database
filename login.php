9aa311b5eb45acc1d0791b7910f4d9c677e90e76
sohanshrestha12 <sohanshrestha40@gmail.com>
2022-09-11 21:26:47 +0545

    login.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+            <input type="text" name="email" placeholder="Enter the email"><br>
+            <input type="password" name="pass" placeholder="Enter the password"><br>
+            <button type="submit">Login</button>
+        </form>
+        <?php 
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            echo "<pre>";
+            print_r($_POST);
+            echo "</pre>";
+            $email =$_POST['email'];
+            $password=$_POST['pass'];
+
+            echo $email . " ";
+            echo $password;
+        }
+        ?>
+</body>
+</html>
