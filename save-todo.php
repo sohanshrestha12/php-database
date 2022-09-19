cb117654c170c08b937963f171d90140f4051773
sohanshrestha12 <sohanshrestha40@gmail.com>
2022-09-19 21:07:09 +0545

    insert

--- save-todo.php
+++ save-todo.php
@@ -10,8 +10,14 @@
 
         $todo_item = $_POST['todo'];
         echo "You have Entered " . $todo_item . " to the list.<br>";
-        echo "<pre>";
-        print_r($conn);
-        echo "</pre>";
+        // echo "<pre>";
+        // print_r($conn);
+        // echo "</pre>";
+
+        $sql_insert = "INSERT INTO todo_items(`title`) VALUES ('$todo_item')";
+        $run = $conn -> query($sql_insert);
+        header("Location: todo.php");
+        die;
+        // exit;
     }
 ?>
